30f3990fce0a380f9acb7224b5a66e09214b5439
Dmitry ST <ebusines2010@gmail.com>
2020-02-20 14:20:00 +0200

    react app

---
+++ react/precache-manifest.4d08dd4471a6c819f99565a731b066fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b333557b03cce6e74c47a4d760146276",
+    "url": "/index.html"
+  },
+  {
+    "revision": "236663beb2ad355920ec",
+    "url": "/static/css/main.b6b98674.chunk.css"
+  },
+  {
+    "revision": "b83d98f10e90f39c4b72",
+    "url": "/static/js/2.a389e6e9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.a389e6e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "236663beb2ad355920ec",
+    "url": "/static/js/main.3f8a0cac.chunk.js"
+  },
+  {
+    "revision": "e457c607676b44d764cf",
+    "url": "/static/js/runtime-main.cd3c8600.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
